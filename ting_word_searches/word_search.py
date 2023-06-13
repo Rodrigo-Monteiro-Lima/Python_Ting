99ddaa76f70cf38957db780612a297a28c5d4b8f
Rodrigo <rodrigoml2395@gmail.com>
2023-06-13 19:18:22 -0300

    feat: search word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -22,7 +22,26 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    arr = []
+    for i in range(len(instance)):
+        current = instance.search(i)
+        dict = {
+            "palavra": word,
+            "arquivo": current["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for phrase in current["linhas_do_arquivo"]:
+            if word.lower() in phrase.lower():
+                dict["ocorrencias"].append(
+                    {
+                        "linha": current["linhas_do_arquivo"].index(phrase)
+                        + 1,
+                        "conteudo": phrase,
+                    }
+                )
+        if dict["ocorrencias"]:
+            arr.append(dict)
+    return arr
 
 
 if __name__ == "__main__":
