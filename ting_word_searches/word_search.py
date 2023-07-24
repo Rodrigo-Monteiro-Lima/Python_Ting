3e66701e6e5974c5f2d24be29dbf7b848d9ccd1a
Rodrigo <rodrigoml2395@gmail.com>
2023-07-24 07:43:28 -0300

    fix: search by word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,40 +1,31 @@
-def exists_word(word, instance):
-    arr = []
+def exists_word(word, instance, phrases=False):
+    occurrences_list = []
     for i in range(len(instance)):
         current = instance.search(i)
-        dict = {
+        result_dict = {
             "palavra": word,
             "arquivo": current["nome_do_arquivo"],
             "ocorrencias": [],
         }
-        for phrase in current["linhas_do_arquivo"]:
+        for idx, phrase in enumerate(current["linhas_do_arquivo"], 1):
             if word.lower() in phrase.lower():
-                dict["ocorrencias"].append(
-                    {"linha": current["linhas_do_arquivo"].index(phrase) + 1}
-                )
-        if dict["ocorrencias"]:
-            arr.append(dict)
-    return arr
+                if phrases:
+                    result_dict["ocorrencias"].append(
+                        {
+                            "linha": idx,
+                            "conteudo": phrase,
+                        }
+                    )
+                else:
+                    result_dict["ocorrencias"].append(
+                        {
+                            "linha": idx,
+                        }
+                    )
+        if result_dict["ocorrencias"]:
+            occurrences_list.append(result_dict)
+    return occurrences_list
 
 
 def search_by_word(word, instance):
-    arr = []
-    for i in range(len(instance)):
-        current = instance.search(i)
-        dict = {
-            "palavra": word,
-            "arquivo": current["nome_do_arquivo"],
-            "ocorrencias": [],
-        }
-        for phrase in current["linhas_do_arquivo"]:
-            if word.lower() in phrase.lower():
-                dict["ocorrencias"].append(
-                    {
-                        "linha": current["linhas_do_arquivo"].index(phrase)
-                        + 1,
-                        "conteudo": phrase,
-                    }
-                )
-        if dict["ocorrencias"]:
-            arr.append(dict)
-    return arr
+    return exists_word(word, instance, True)
